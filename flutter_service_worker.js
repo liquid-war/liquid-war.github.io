495cf6e10ab954db1fb5e7e3d6d3ca54ec9ba16e
TeemuKoivisto <TeemuKoivisto@users.noreply.github.com>
2023-03-29 19:17:02 +0000

    deploy: liquid-war/liquid-battle@c9d149fecf7d2a362f0f80878e9522e790e2011b

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,7 +4,7 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
-"main.dart.js": "f2afff759312989b9b33c036ea5abf20",
+"main.dart.js": "e1187dc7cf2ea316ecbfaad98f80d219",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
@@ -19,8 +19,8 @@ const RESOURCES = {
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "18992576bf3a6561d698ec080eb321fd",
 "assets/NOTICES": "b2db90b6b94af8500fe3877052c1e561",
-"index.html": "593d78d622dbe9ecea35fa3347b94be6",
-"/": "593d78d622dbe9ecea35fa3347b94be6",
+"index.html": "83355c653edb650f1617f3fa23da2dc4",
+"/": "83355c653edb650f1617f3fa23da2dc4",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
